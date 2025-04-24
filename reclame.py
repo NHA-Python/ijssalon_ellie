6f645526746f31a1c5ed1534aaa4a5ca6814aeeb
Rick Pijnenburg <pijnenburg.rick@gmail.com>
2025-04-24 20:43:23 +0200

    Les 8 huiswerk

--- reclame.py
+++ reclame.py
@@ -42,4 +42,4 @@ def combinatie(invoer_lijst_2):
     uitvoer = mijn_functie_2(korte_lijst[0],korte_lijst[1])
     return uitvoer
 
-print(combinatie([10,5,3,2,1,2,9]))
+# print(combinatie([10,5,3,2,1,2,9]))
