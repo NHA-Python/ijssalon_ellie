b65c7a8d94d2bcb926250540be816c766842f250
Rick Pijnenburg <pijnenburg.rick@gmail.com>
2025-04-27 20:32:40 +0200

    Les 10: Boekhouding en presentatie toegevoegd

--- helper.py
+++ helper.py
@@ -6,3 +6,19 @@ def decoreer(tekst = ""):
     print(lengte * "*")
     print()
 
+def fooi_pp(bedrag, personen):
+    bedrag_pp = bedrag / personen
+    bedrag_pp = "??"
+    return f"Het bedrag per persoon is {bedrag_pp}"
+
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+
+def som(dict):
+    uitkomst = 0
+    for k, v in dict.items():
+        uitkomst += int(v)
+    return uitkomst
---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer(dict, totaal):
+    for key in dict:
+        print(f"{key} : {dict[key]} euro")
+
+    print("=" * 26)
+    print(f"totaal : {totaal} euro")
+
